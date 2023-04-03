29f69774be042edf424a59a7337743430af2a559
Will Orsetti <worsett@gmail.com>
2023-04-03 15:32:22 -0600

    Agrega Panatalla roja

--- app/src/main/java/com/willmer/gamermvvmapp/MainActivity.kt
+++ app/src/main/java/com/willmer/gamermvvmapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.willmer.gamermvvmapp
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.willmer.gamermvvmapp.ui.theme.GammerMVVMAppTheme
@@ -32,11 +41,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BodyContent() {
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .wrapContentHeight()
+            .background(color = Color.Red)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Image(painter = painterResource(id = R.drawable.control ) ,
             contentDescription ="Control Xbox360"
         )
         Text(text = "Firebase MVVM")
+        Text(text = "LOGIN")
+        Text(text = "Por favor inicia sesiòn para continuar")
     }
 }
 
