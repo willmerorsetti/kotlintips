a33b792a3b5d18fe1e1b1e8a44af909c8b3e1b0e
Will Orsetti <worsett@gmail.com>
2023-04-03 16:37:22 -0600

    Se agregan textfield

--- app/src/main/java/com/willmer/gamermvvmapp/MainActivity.kt
+++ app/src/main/java/com/willmer/gamermvvmapp/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,6 +20,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.willmer.gamermvvmapp.ui.theme.GammerMVVMAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,8 +43,8 @@ fun BodyContent() {
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .wrapContentHeight()
-            .background(color = Color.Red)
+            //.wrapContentHeight()
+            //.background(color = Color.Red)
         ,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -53,14 +52,39 @@ fun BodyContent() {
             contentDescription ="Control Xbox360"
         )
         Text(text = "Firebase MVVM")
-        Text(text = "LOGIN")
+        Text(
+            //modifier = Modifier.padding(25.dp),
+            modifier = Modifier.padding(top = 30.dp, bottom = 0.dp, start = 0.dp, end = 0.dp),
+            text = "LOGIN"
+        )
+        Spacer(modifier = Modifier.height(10.dp))
         Text(text = "Por favor inicia sesiòn para continuar")
+        TextField(
+                  modifier = Modifier.padding(top= 25.dp),
+                  value = "",
+                  onValueChange = {},
+            placeholder = {
+                Text(text = "Correo electronico")
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "Contraseña")
+            }
+        )
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        //modifier = Modifier.padding(25.dp),
+        modifier = Modifier.padding(top = 30.dp),
+        text = "Hello $name!"
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
