910645ccbbb4499a76d942787ec0c9d07814f4a5
Will Orsetti <worsett@gmail.com>
2023-04-03 23:12:47 -0600

    Terminado diseño pantalla

--- app/src/main/java/com/willmer/gamermvvmapp/screens/login/Components/loginContent.kt
+++ app/src/main/java/com/willmer/gamermvvmapp/screens/login/Components/loginContent.kt
@@ -2,8 +2,14 @@ package com.willmer.gamermvvmapp.screens.login.Components
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,59 +21,131 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.willmer.gamermvvmapp.R
 import com.willmer.gamermvvmapp.screens.login.LoginScreen
+import com.willmer.gamermvvmapp.ui.theme.Darkgray500
+import com.willmer.gamermvvmapp.ui.theme.Darkgray700
 import com.willmer.gamermvvmapp.ui.theme.GammerMVVMAppTheme
+import com.willmer.gamermvvmapp.ui.theme.Red500
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun LoginContent () {
-    Column(
+    Box(
         modifier = Modifier
-            .padding()
             .fillMaxHeight()
         //.wrapContentHeight()
         //.background(color = Color.Red)
-        ,
-        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Image(
+        BoxHeader()
+        CardForm()
+    }
+
+
+}
+
+@Composable
+fun BoxHeader() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(280.dp)
+        .background(Red500)
+    ){
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
             modifier = Modifier.height(130.dp),
             painter = painterResource(id = R.drawable.control ) ,
             contentDescription ="Control Xbox360"
         )
-        Text(text = "Firebase MVVM")
-        Text(
-            //modifier = Modifier.padding(25.dp),
-            modifier = Modifier.padding(top = 30.dp, bottom = 0.dp, start = 0.dp, end = 0.dp),
-            text = "LOGIN"
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Por favor inicia sesiòn para continuar")
-        TextField(
-            modifier = Modifier.padding(top= 25.dp),
-            value = "",
-            onValueChange = {},
-            placeholder = {
-                Text(text = "Correo electronico")
-            }
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        TextField(
-            value = "",
-            onValueChange = {},
-            placeholder = {
-                Text(text = "Contraseña")
-            }
-        )
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 30.dp, vertical = 15.dp),
-            onClick = {  }) {
-            Text(text = "INICIAR SESION")
+            Text(text = "Firebase MVVM")
         }
 
     }
+}
 
+@Composable
+fun CardForm(){
+    Card(modifier =Modifier.padding(start =40.dp, end = 40.dp, top = 170.dp ),
+        backgroundColor = Darkgray700
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 20.dp)
+        ) {
+            Text(
+                //modifier = Modifier.padding(25.dp),
+                modifier = Modifier.padding(
+                    top = 40.dp,
+                    bottom = 0.dp,
+                    start = 0.dp,
+                    end = 0.dp
+                ),
+                text = "LOGIN",
+                fontSize = 19.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                //modifier = Modifier.padding(
+                //top = 30.dp,
+                //bottom = 0.dp,
+                //start = 0.dp,
+                //end = 0.dp
+                //),
+                text = "Por favor inicia sesiòn para continuar",
+                fontSize = 16.sp,
+                //fontWeight = FontWeight.Bold,
+                color=Color.Gray,
+            )
+            OutlinedTextField(
+                modifier = Modifier.padding(top= 25.dp),
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(text = "Correo electronico")
+                },
+                //trailingIcon = {
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Email,
+                         contentDescription ="",
+                         tint = Color.White
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(text = "Contraseña")
+                }
+                ,
+                //trailingIcon = {
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Lock,
+                        contentDescription ="",
+                        tint = Color.White
+                    )
+                }
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 45.dp),
+                onClick = {  }) {
+                Icon(imageVector = Icons.Default.ArrowForward,
+                    contentDescription ="",
+                    tint = Color.White
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "INICIAR SESION")
+            }
+        }
+
+
+    }
 
 }
 
